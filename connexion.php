2cb1c25e0227bedd654ad374022e49e45e0d0333
Florian De Saint Leger <de.saint.leger.florian@gmail.com>
2014-11-25 17:21:55 +0100

    Acces multiplateforme

--- connexion.php
+++ connexion.php
@@ -1,5 +1,22 @@
 <?php
-header('Location: Admin/index.php');      
+//header('Location: Admin/index.php');      
 
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+   <link rel="shortcut icon" type="image/ico" href="img/favicon.gif" />
+   
+   <title> titre </title>
+ </head>
+ <body>
+
+ <A HREF="Admin/index.php">Accés Admin</A> 
+ <A HREF="Customer/index.php">Accés Client</A> 
+ </body>
+</html>
