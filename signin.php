77da9e71b45b4dc7cf6cd753c62db151c4d17433
Charles Lamarque <chabox59@gmail.com>
2014-11-25 16:15:32 +0100

    ajout signin

---
+++ signin.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="img/c.ico">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Bootstrap core CSS -->
+    <link href="https://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+
+				<div class="login-panel">
+					<h3 id="title">S'inscrire</h3>
+					<form action="" method="post">
+	
+					<input type="text" class="form-control" placeholder="Nom" required=""  name="nom" id="field1">
+                    <input type="text" class="form-control" placeholder="prénom" required="" name="prenom" id="field2">
+                    <input type="email" class="form-control" placeholder="adresse mail" required="" name="mail" id="field3">
+                    <input type="password" class="form-control" placeholder="mot de passe" required="" name="pass" id="field4">
+
+                       
+                    <input  class="btn-connect btn btn-warning" type="submit" value="Me connecter" /><br/>
+                 
+                      </form>
+				</div>
+
+			</div>
+		</div>
+
+	</div>
+
+</body>
+</html>
