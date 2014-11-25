f70dd5d831b20974c5f4fbf106584574562ab65b
Florian De Saint Leger <de.saint.leger.florian@gmail.com>
2014-11-25 15:42:13 +0100

    Test OK mise en commentaire de la session Création de la connexion.php

--- index.php
+++ index.php
@@ -1,31 +1,4 @@
 <?php
-session_start();
-
-//on vérifie que le login/mdp sont bien envoyé
-//on vérifie que le login/mdp ne sont pas vide
-if(isset($_POST) && !empty($_POST['login']) && !empty($_POST['pass'])) {
-	extract($_POST);
-	/*$pass = sha1($pass);*/
-	mysql_connect("localhost","root","");
-	mysql_select_db("clamarque");
-	$sql = " SELECT * FROM invites where login='$login' AND pass='$pass'";
-$req = mysql_query($sql) or die (mysql_error());
-//indique le nombre de resultat trouvée
-if( mysql_num_rows($req)>0) {
-	$data = mysql_fetch_assoc($req);
-$_SESSION['Auth'] = array(
-'login' => $login,
-'pass' => $pass
-/*
-'role' => $data['role']*/
-);
-
-header('Location:menu.php');
-}
-else{
-	 $merreur="<h3 class=error-login>Mauvais identifiants</h3>";
- }
-}
 
 ?>
 <!DOCTYPE html>
@@ -47,7 +20,7 @@ else{
 
 				<div class="login-panel">
 					<h3 id="title">EPSI IMMO</h3>
-					<form action="index.php" method="post">
+					<form action="connexion.php" method="post">
 					<?php if (isset($merreur)) echo $merreur; ?>
 					<input type="text" class="form-control" placeholder="Email address" required=""  name="login" id="field1">
                         <input type="password" class="form-control" placeholder="Password" required="" name="pass" id="field2">
