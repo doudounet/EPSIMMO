8fe371cc0c88e53cc872087493de5d013badac18
Ludovic Mantovani <ludovic.mantov@gmail.com>
2014-11-25 15:34:52 +0100

    Ajout champ mdp dans la table user

--- scriptSQL/create_structure.sql
+++ scriptSQL/create_structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 25 Novembre 2014 à 15:11
+-- Généré le :  Mar 25 Novembre 2014 à 15:33
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `epsimmo`
 --
+CREATE DATABASE IF NOT EXISTS `epsimmo` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `epsimmo`;
 
 -- --------------------------------------------------------
 
@@ -40,6 +42,7 @@ CREATE TABLE IF NOT EXISTS `client` (
   `cp` varchar(5) NOT NULL,
   `ville` varchar(50) NOT NULL,
   `idtype` int(11) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `idsecret` (`idsecret`),
   KEY `idtype` (`idtype`)
@@ -92,8 +95,8 @@ CREATE TABLE IF NOT EXISTS `type` (
 -- Contraintes pour la table `client`
 --
 ALTER TABLE `client`
-  ADD CONSTRAINT `client_ibfk_2` FOREIGN KEY (`idtype`) REFERENCES `type` (`id`),
-  ADD CONSTRAINT `client_ibfk_1` FOREIGN KEY (`idsecret`) REFERENCES `secret` (`id`);
+  ADD CONSTRAINT `client_ibfk_1` FOREIGN KEY (`idsecret`) REFERENCES `secret` (`id`),
+  ADD CONSTRAINT `client_ibfk_2` FOREIGN KEY (`idtype`) REFERENCES `type` (`id`);
 
 --
 -- Contraintes pour la table `connexion`
