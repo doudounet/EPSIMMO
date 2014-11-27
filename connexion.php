c10b049e48843115313cf5d59891abf952bd4b36
Florian De Saint Leger <de.saint.leger.florian@gmail.com>
2014-11-27 11:50:06 +0100

    no message

--- connexion.php
+++ connexion.php
@@ -1,22 +1,70 @@
 <?php
-//header('Location: Admin/index.php');      
-include("class/UserManager.php");
+//header('Location: Admin/index.php'); 
+   
+//require('class/UserManager.php');
 
 $login = $_POST['login'];
 $pass = $_POST['pass'];
-$db = new PDO('mysql:host=cloud.mylittlestorage.fr;dbname=epsimmo', 'epsimmo', 'epsimmo');       
-$manager = new UserManager($db);
-$manager->verifcon($login,$pass);
+ 
+$u = 'epsimmo';
+$p = 'epsimmo';
+try 
+{
+
+    $db = new PDO('mysql:host=localhost;dbname=epsimmo', $u, $p);
+    
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+echo 'connexion ok';
+
+try 
+{
+
+    $manager = $db->query("SELECT COUNT(*) AS ok FROM client WHERE email='$login' AND mdp='$pass'")->fetchColumn();
+    
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+try 
+{
+
+	$resultats=$db->query("SELECT idtype FROM client WHERE email='$login' AND mdp='$pass'");
+	$resultats->setFetchMode(PDO::FETCH_OBJ);
+	
+while( $resultat = $resultats->fetch() )
+{
+        $droit = $resultat->idtype; 
+}
+$resultats->closeCursor();
+
+echo $droit;
+    
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+echo($manager);
 
 if($manager == 1)
 {
-echo('OK'); 
-header('Admin/index.php'); 
+	if($droit ==1)
+	{
+	header('Location: Admin/index.php');   
+	}
+	else
+	{
+	header('Location: Customer/index.php');
+	}
 }
 else
 {
-
-echo('erreur'); 
+header('Location: index.php');
 }
 
 ?>
